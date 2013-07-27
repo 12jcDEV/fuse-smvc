4cb31a08423948cd5608d41046bbadd2133f9ef7
12jcDEV <jayce1772@live.com>
2013-07-27 21:00:39 +0800

    initial commit

---
+++ src/main/java/org/adb/service/impl/LookupManagerImpl.java
@@ -0,0 +1,37 @@
+package org.adb.service.impl;
+
+import org.adb.dao.LookupDao;
+import org.adb.model.LabelValue;
+import org.adb.model.Role;
+import org.adb.service.LookupManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Implementation of LookupManager interface to talk to the persistence layer.
+ *
+ * @author <a href="mailto:matt@raibledesigns.com">Matt Raible</a>
+ */
+@Service("lookupManager")
+public class LookupManagerImpl implements LookupManager {
+    @Autowired
+    LookupDao dao;
+
+    /**
+     * {@inheritDoc}
+     */
+    public List<LabelValue> getAllRoles() {
+        List<Role> roles = dao.getRoles();
+        List<LabelValue> list = new ArrayList<LabelValue>();
+
+        for (Role role1 : roles) {
+            list.add(new LabelValue(role1.getName(), role1.getName()));
+        }
+
+        return list;
+    }
+}
